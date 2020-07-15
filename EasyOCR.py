5a2f10813c4d701566efae489cbd19bc4dd3208d
QinDongyi <45354159+QinDongyi@users.noreply.github.com>
2020-07-15 17:24:24 +0800

    Update EasyOCR.py

--- EasyOCR.py
+++ EasyOCR.py
@@ -1,6 +1,6 @@
 import pytesseract
 from PIL import Image
-img = Image.open('exo.jpg')
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\dell\Desktop\TextBook_\手工工作\知识清单OCR\tesseract\Tesseract-OCR\tesseract.exe'
+img = Image.open('exo.jpg') # 放你自己的图片
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\dell\Desktop\TextBook_\tesseract\Tesseract-OCR\tesseract.exe'
 s = pytesseract.image_to_string(img, lang='chi_sim')  #不加lang参数的话，默认进行英文识别
 print(s)
